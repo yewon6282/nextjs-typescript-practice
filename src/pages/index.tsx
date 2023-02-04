e379b3fa1634b94d14cab849191199bab26cdff0
Kimyewon <yewon6282@gmail.com>
2023-02-04 21:28:02 +0900

    FEAT : Atom Effects 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import ShowList from "@/components/ShowList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { listState } from "@/store/listState";
 
 const Home = () => {
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const [value, setValue] = useState<string>("");
   const [countIndex, setCountIndex] = useState<number>(0);
   const [list, setList] = useRecoilState(listState);
@@ -19,23 +25,26 @@ const Home = () => {
   };
 
   return (
-    <div className="w-80 border-solid border-2 border-black">
-      <div className="w-full">
-        <input type="text" className="w-4/5 border-solid border-2 border-sky-500 rounded-lg p-2" onChange={(e) => inputValue(e.target.value)} value={value || ""}></input>
-        <button className="w-1/5 rounded-lg p-2" onClick={addValue}>
-          입력
-        </button>
-      </div>
+    mounted && (
+      <div className="w-80 p-2">
+        <div className="w-full">
+          <input type="text" className="w-4/5 border-solid border-2 border-sky-500 rounded-lg p-2" onChange={(e) => inputValue(e.target.value)} value={value || ""}></input>
+          <button className="w-1/5 rounded-lg p-2" onClick={addValue}>
+            입력
+          </button>
+        </div>
 
-      <ul>
-        {list.length > 0 &&
-          list.map((el) => (
-            <li key={el.index}>
-              <ShowList data={el} setList={setList} />
-            </li>
-          ))}
-      </ul>
-    </div>
+        {list.length > 0 && (
+          <ul>
+            {list.map((el) => (
+              <li key={el.index}>
+                <ShowList data={el} setList={setList} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    )
   );
 };
 
--- src/store/listState.tsx
+++ src/store/listState.tsx
@@ -1,3 +1,36 @@
+// import { atom } from "recoil";
+
+// export type toDoList = {
+//   index: number;
+//   value: string;
+// };
+
+// const asyncUserListEffect =
+//   (key: string) =>
+//   ({ onSet, setSelf }: any) => {
+//     setSelf(() => {
+//       if (typeof window !== "undefined") {
+//         const localData = localStorage.getItem(key);
+//         if (localData !== null) {
+//           return JSON.parse(localData);
+//         } else {
+//           return [];
+//         }
+//       }
+//     });
+
+//     onSet((newValue: any, _: any, isReset: boolean) => {
+//       localStorage.setItem(key, JSON.stringify(newValue));
+//     });
+//   };
+
+// const listState = atom<toDoList[]>({
+//   key: "listState",
+//   effects: [asyncUserListEffect("listState")],
+// });
+
+// export { listState };
+
 import { atom } from "recoil";
 
 export type toDoList = {
@@ -5,9 +38,23 @@ export type toDoList = {
   value: string;
 };
 
-const listState = atom<toDoList[]>({
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    if (typeof window !== "undefined") {
+      const savedValue = localStorage.getItem(key);
+      if (savedValue != null) {
+        setSelf(JSON.parse(savedValue));
+      }
+    }
+
+    onSet((newValue: any, _: any, isReset: boolean) => {
+      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
+export const listState = atom<toDoList[]>({
   key: "listState",
   default: [],
+  effects: [localStorageEffect("listState")],
 });
-
-export { listState };
